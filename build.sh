0847cb2105bf072447b88d04516ec3ea14b30142
Peter Kukla <fruviad@yahoo.com>
2021-10-08 01:59:03 +0000

    Improve logging and misc..WIP

--- build.sh
+++ build.sh
@@ -5,17 +5,32 @@
 #            calls:  buildstatic.sh
 #
 
-RUNDATE=`date +%Y-%m-%d_%H-%m-%S`
 
-echo "Running Combine-Docker build script.  Note: this may take some time, anywhere from 5-20 minutes depending on your hardware."
+
+RUNDATE=`date +%Y-%m-%d_%H-%m-%S`
 
 # setup base directory for this build script
 WORKDIR=$(pwd)
 
-# source .env file to load config information that will probably change from release to release
+
+# source .env file to load some config information; most will probably change from release to release:
+#
+#   COMBINE_BRANCH=v0.11.1
+#   COMBINE_DJANGO_HOST_PORT=8000
+#   COMBINE_DOCKER_VERSION=v0.11
+#   ELASTICSEARCH_HADOOP_CONNECTOR_VERSION=7.1.1
+#   HADOOP_VERSION=2.7.5
+#   HADOOP_VERSION_SHORT=2.7
+#   LIVY_TAGGED_RELEASE=v0.6.0-incubating
+#   SCALA_VERSION=2.11
+#   SPARK_VERSION=2.3.2
+#   SPARK_GIT=branch-2.3
 source $WORKDIR/.env
-echo "### BUILDLOG:  Base install directory (WORKDIR) is:  $WORKDIR"
-sleep 1
+
+
+# override "COMBINE_BRANCH" version with dev value
+COMBINE_BRANCH='update-installer'
+
 # we will log output in output files in subdirectories named according to date-time of build run
 if [ ! -d $WORKDIR/buildlogs ]
 then
@@ -26,7 +41,20 @@ BUILDLOGDIR=$WORKDIR/buildlogs/build-log_$RUNDATE
 mkdir $BUILDLOGDIR
 BUILDLOG=$BUILDLOGDIR/build-log_$RUNDATE.log
 
+echo ""
+echo ""
 
+echo "####################################################################################################"
+echo "### BUILDLOG:  Running Combine-Docker build script. "
+echo "### BUILDLOG:  Note: this may take some time, anywhere from 5-20 minutes depending on your hardware."
+echo "### BUILDLOG"
+echo "### BUILDLOG:  Base install directory (WORKDIR) is:  $WORKDIR" | tee -a $BUILDLOG
+echo "### BUILDLOG:  " | tee -a $BUILDLOG
+echo "### BUILDLOG:  see logs as they're created:  tail -f $BUILDLOG "
+echo "### BUILDLOG:"
+echo "### BUILDLOG:  hit ENTER to continue..."
+read ANSWER
+echo "### BUILDLOG:  Base install directory (WORKDIR) is:  $WORKDIR" | tee -a $BUILDLOG
 
 # bring down Combine docker containers, if running
 echo "### BUILDLOG:  Bringing down Docker containers (if they're running)"  | tee -a $BUILDLOG
@@ -39,15 +67,16 @@ if [[ ! -f "$WORKDIR/nginx/nginx.conf" ]]; then
 fi
 
 # init Combine app as a git submodule and use localsettings docker template
-echo "Initializing Combine app submodule" 2>&1 | tee -a $BUILDLOG
-#echo $COMBINE_BRANCH 2>&1 | tee -a $BUILDLOG
+echo "### BUILDLOG:     Initializing Combine app submodule" 2>&1 | tee -a $BUILDLOG
+echo "### BUILDLOG:     Working with Combine git branch:  $COMBINE_BRANCH" 2>&1 | tee -a $BUILDLOG
 git submodule init   2>&1 | tee -a $BUILDLOG
 git submodule update 2>&1 | tee -a $BUILDLOG
 
+echo "### BUILDLOG:  Fetching desired Combine git branch" 2>&1 | tee -a $BUILDLOG
 cd $WORKDIR/combine/combine 
 git fetch 2>&1 | tee -a $BUILDLOG
 git checkout $COMBINE_BRANCH 2>&1 | tee -a $BUILDLOG
-##### git pull  # syntax incorrect; original intent unclear?
+git pull
 
 # the $WORKDIR/combine/combine/combine/localsettings.py and localsettings.py.docker
 # files contain a variety of configuration settings for the Combine components
@@ -66,22 +95,28 @@ cd $WORKDIR
 
 
 # build images
-echo
+echo 
 echo 
 echo "### BUILDLOG:  Removing existing Docker images" 2>&1 | tee -a $BUILDLOG
 docker volume rm -f combine-docker_combine_python_env combine-docker_hadoop_binaries combine-docker_spark_binaries combine-docker_livy_binaries combine-docker_combine_tmp combine-docker_combinelib  | tee -a $BUILDLOG
 
 echo  2>&1 | tee -a $BUILDLOG
-echo "### BUILDLOG:  running docker-compose build in 2 seconds" 2>&1 | tee -a $BUILDLOG
-sleep 2
-docker-compose build 2>&1 >> $BUILDLOG
+
+echo "### BUILDLOG:  running docker-compose build...download can hang for several minutes" 2>&1 | tee -a $BUILDLOG
+echo "### BUILDLOG:  start time:  `date`"  2>&1 | tee -a $BUILDLOG
+docker-compose build  2>&1 >> $BUILDLOG
+echo "### BUILDLOG:  finish time:  `date`"  2>&1 | tee -a $BUILDLOG
 echo "### BUILDLOG:  finished running docker-compose build" 2>&1 | tee -a $BUILDLOG
 
 # format Hadoop namenode
-#####docker-compose run hadoop-namenode /bin/bash -c "mkdir -p /hdfs/namenode"
-#####docker-compose run hadoop-namenode /bin/bash -c "echo 'Y' | /opt/hadoop/bin/hdfs namenode -format"
+echo "### BUILDLOG:  formatting hadoop-namenode" 2>&1 | tee -a $BUILDLOG
+docker-compose run hadoop-namenode /bin/bash -c "mkdir -p /hdfs/namenode" 1>>$BUILDLOG 2>&1
+docker-compose run hadoop-namenode /bin/bash -c "echo 'Y' | /opt/hadoop/bin/hdfs namenode -format"  1>>$BUILDLOG 2>&1
+
 
 # Combine db migrations and superuser create
-#####docker-compose run combine-django /bin/bash -c "bash /tmp/combine_db_prepare.sh"
+echo "### BUILDLOG:  Combine db migrations and creation of superuser" 2>&1 | tee -a $BUILDLOG
+docker-compose --log-level=warning run combine-django /bin/bash -c "bash /tmp/combine_db_prepare.sh" 2>&1 | tee -a $BUILDLOG
 
-#####. $WORKDIR/buildstatic.sh
+echo "### BUILDLOG:  Passing control to buildstatic.sh" 2>&1 | tee -a $BUILDLOG
+. $WORKDIR/buildstatic.sh
--- buildstatic.sh
+++ buildstatic.sh
@@ -1,33 +1,53 @@
 source ./.env
 WORKDIR=$(pwd)
 
+echo "### BUILDLOG:  Creating static untar" 2>&1 | tee -a $BUILDLOG
 cd $WORKDIR/combine/combine
 if [[ ! -d "./static/" ]]; then
   mkdir static/
 fi
-tar -cvf static.tar ./core/static/*
+tar -cf static.tar ./core/static/*
 mv static.tar ./static
 cd ./static
-tar -xvf static.tar --strip=5
-tar -xvf static.tar --strip=4
-tar -xvf static.tar --strip=3
-tar -xvf static.tar --strip=2
-tar -xvf static.tar --strip=1
+echo "### BUILDLOG:  Starting untar static.tar --strip=5" 2>&1 | tee -a $BUILDLOG
+tar -xf static.tar --strip=5
+echo "### BUILDLOG:  Starting untar static.tar --strip=4" 2>&1 | tee -a $BUILDLOG
+tar -xf static.tar --strip=4
+echo "### BUILDLOG:  Starting untar static.tar --strip=3" 2>&1 | tee -a $BUILDLOG
+tar -xf static.tar --strip=3
+echo "### BUILDLOG:  Starting untar static.tar --strip=2" 2>&1 | tee -a $BUILDLOG
+tar -xf static.tar --strip=2
+echo "### BUILDLOG:  Starting untar static.tar --strip=1" 2>&1 | tee -a $BUILDLOG
+tar -xf static.tar --strip=1
+
 if [[ ! -d "$WORKDIR/external-static/livy/" ]]; then
   mkdir -p $WORKDIR/external-static/livy
 fi
 cd $WORKDIR/external-static/livy
+
+echo "### BUILDLOG:  svn export livy" 2>&1 | tee -a $BUILDLOG
 svn export --force https://github.com/apache/incubator-livy/tags/$LIVY_TAGGED_RELEASE/server/src/main/resources/org/apache/livy/server/ui/static/js/
+
+echo "### BUILDLOG:  svn export spark" 2>&1 | tee -a $BUILDLOG
 if [[ ! -d "$WORKDIR/external-static/spark/" ]]; then
   mkdir -p $WORKDIR/external-static/spark
 fi
 cd $WORKDIR/external-static/spark
 svn export --force https://github.com/apache/spark/tags/v$SPARK_VERSION/core/src/main/resources/org/apache/spark/ui/static
 cd $WORKDIR
-tar -cvf external-static.tar ./external-static/*
+
+echo "### BUILDLOG:  creating external-static.tar" 2>&1 | tee -a $BUILDLOG
+tar -cf external-static.tar ./external-static/*
 mv external-static.tar $WORKDIR/combine/combine/static
 cd $WORKDIR/combine/combine/static
-tar -xvf external-static.tar --strip=3
-tar -xvf external-static.tar --strip=2
-tar -xvf external-static.tar --strip=1
+
+echo "### BUILDLOG:  Starting external-static untar --strip=3" 2>&1 | tee -a $BUILDLOG
+tar -xf external-static.tar --strip=3
+echo "### BUILDLOG:  Starting external-static untar --strip=2" 2>&1 | tee -a $BUILDLOG
+tar -xf external-static.tar --strip=2
+echo "### BUILDLOG:  Starting external-static untar --strip=1" 2>&1 | tee -a $BUILDLOG
+tar -xf external-static.tar --strip=1
+
 cd $WORKDIR
+echo "### BUILDLOG:  buildstatic.sh complete" 2>&1 | tee -a $BUILDLOG
+
